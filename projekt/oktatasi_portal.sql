04bf0cd72d0072a6a44853dad37c88cb39008853
Szobonya Norbert <159431750+szobonyan@users.noreply.github.com>
2025-01-13 19:56:51 +0100

    Update oktatasi_portal.sql

--- projekt/oktatasi_portal.sql
+++ projekt/oktatasi_portal.sql
@@ -34,3 +34,17 @@ CREATE TABLE `tananyag` (
   `szerzo` int(255) NOT NULL,
   `kategoria` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `permission` varchar(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
