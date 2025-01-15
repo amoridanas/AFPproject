b2b27261fba7ef3ddc9f4db8cb885a2fc975472b
amoridanas <amoridanas5@gmail.com>
2025-01-15 20:48:07 +0100

    database.php refaktorálása

--- projekt/database.php
+++ projekt/database.php
@@ -1,46 +1,43 @@
 <?php
-    define('dbHOST','localhost');
-    define('dbUSERNAME','root');
-    define('dbPASSWORD','');
-    define('dbDATABASE','oktatasi_portal');
-    define('dbPORT','3306');
+define('dbHOST', 'localhost');
+define('dbUSERNAME', 'root');
+define('dbPASSWORD', '');
+define('dbDATABASE', 'oktatasi_portal');
+define('dbPORT', '3306');
 
-    function connect()
-    {
-        $connection = mysqli_connect(dbHOST,dbUSERNAME,dbPASSWORD,dbDATABASE,dbPORT);
-        if(!$connection)
-        {
-            die("Hiba a kapcsolat kiépítés során: ".mysqli_connect_error());
-        }
-        return $connection;
+function connect()
+{
+    $connection = mysqli_connect(dbHOST, dbUSERNAME, dbPASSWORD, dbDATABASE, dbPORT);
+    if (!$connection) {
+        die("Hiba a kapcsolat kiépítés során: " . mysqli_connect_error());
     }
-    function add_user($username,$email,$password,$permission)
-    {
-        $connection = connect();
-        $query = "INSERT INTO users (username,email,password,permission)
-                  VALUES ('$username','$email','$password','$permission')";
-                  mysqli_query($connection, $query) or die("Hiba a lekérdezésben: $query");
-                  mysqli_close($connection);
-                  header("Location: index.php?page=1&msg=Sikeres regisztráció!");
+    return $connection;
+}
+function add_user($username, $vezeteknev, $keresztnev, $szuletesi_datum, $email, $password, $permission)
+{
+    $connection = connect();
+    $query = "INSERT INTO users (username,vezeteknev,keresztnev,szuletesi_datum,email,password,permission)
+                  VALUES ('$username','$vezeteknev','$keresztnev','$szuletesi_datum','$email','$password','$permission')";
+    mysqli_query($connection, $query) or die("Hiba a lekérdezésben: $query");
+    mysqli_close($connection);
+    header("Location: index.php?page=1&msg=Sikeres regisztráció!");
+}
+function login($username, $password)
+{
+    $connection = connect();
+    $query = "SELECT * FROM users WHERE username LIKE '$username' AND password LIKE '$password'";
+    $result = mysqli_query($connection, $query) or die("Hiba a lekérdezésben: $query");
+    $users = mysqli_fetch_row($result);
+    if (!$users) {
+        header("Location: login.php?page=1&error=Hibás+adatok");
+    } else {
+        session_start();
+        $_SESSION['id'] = $users[0];
+        $_SESSION['username'] = $users[1];
+        $_SESSION['permission'] = $users[7];
+        header("Location: index2.php");
     }
-    function login($username,$password) {
-        $connection = connect();
-        $query = "SELECT * FROM users WHERE username LIKE '$username' AND password LIKE '$password'";
-        $result = mysqli_query($connection, $query) or die("Hiba a lekérdezésben: $query");
-        $users = mysqli_fetch_row($result);
-        if(!$users) 
-        {
-            header("Location: login.php?page=1&error=Hibás+adatok");
-        }
-        else 
-        {
-            session_start();
-            $_SESSION['id'] = $users[0];
-            $_SESSION['username'] = $users[1];
-            $_SESSION['permission'] = $users[4];
-            header("Location: index2.php");
-        }
-        mysqli_close($connection);
-    }
+    mysqli_close($connection);
+}
 
 ?>
