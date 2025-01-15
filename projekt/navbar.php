d930031da2e3758f7e46577c4cde5ee68e93c90b
amoridanas <amoridanas5@gmail.com>
2025-01-15 20:42:53 +0100

    új oldalak beszúrása

--- projekt/navbar.php
+++ projekt/navbar.php
@@ -1,50 +1,33 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="stiluslap.css">
-    <title>Document</title>
-</head>
-<body>
-
-<?php 
-session_start();
-
-if(!isset($_SESSION['id']) || empty($_SESSION['id'])):
-    header("Location: login.php");
-    exit();
-endif;
-?>
-
-<?php
-if(isset($_SESSION['message']) && !empty($_SESSION['message'])):
-?>
-    <div class="message">
-        <?php echo $_SESSION['message']; ?>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+        <a class="navbar-brand" href="index.php">  </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav justify-content-center w-100">
+                <?php if (!isset($_SESSION['id']) || empty($_SESSION['id'])): ?>
+                    <li class="nav-item"><a class="nav-link" href="login.php">Bejelentkezés</a></li>
+                    <li class="nav-item"><a class="nav-link" href="register.php">Regisztráció</a></li>
+                <?php else: ?>
+                    <li class="nav-item"><a class="nav-link" href="index.php">Főoldal</a></li>
+                    <li class="nav-item"><a class="nav-link" href="help.php">Kapcsolat</a></li>
+                    <?php if ($_SESSION['permission'] === "admin"): ?>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="adminDropdown" role="button"
+                                data-bs-toggle="dropdown" aria-expanded="false">
+                                Adminisztrátor</a>
+                            <ul class="dropdown-menu" aria-labelledby="adminDropdown">
+                                <li><a class="dropdown-item" href="create_courses.php">Tananyag létrehozása</a></li>
+                                <li><a class="dropdown-item" href="manage_courses.php">Tananyagok kezelése</a></li>
+                            </ul>
+                        </li>
+                    <?php endif; ?>
+                    <li class="nav-item"><a class="nav-link" href="modifyMyProfile.php">Profilom kezelése</a></li>
+                    <li class="nav-item"><a class="nav-link" href="logout.php">Kijelentkezés</a></li>
+                <?php endif; ?>
+            </ul>
+        </div>
     </div>
-<?php
-endif;
-?>
-
-<nav class="navigacio">
-    <?php if(!isset($_SESSION['id']) || empty($_SESSION['id'])): ?>
-        <a href="index.php?page=1">Bejelentkezés</a>
-        <a href="index.php?page=2">Regisztráció</a>
-    <?php else: ?>
-        <?php if($_SESSION['permission'] == "admin"): ?>
-            <a href="newsManagement.php">Tananyagok kezelése</a> 
-        <?php endif; ?>
-        <a href="modifyMyProfile.php">Profilom kezelése</a>
-        <a href="createArticle.php">Tananyag létrehozása</a>
-        <a href="logout.php">Kijelentkezés</a>
-    <?php endif; ?>
 </nav>
-
-
-<div class="main">
-
-</div>
-</body>
-</html>
