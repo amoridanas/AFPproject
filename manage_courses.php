a0ed7763ba98e3c97923ac74c917922022b7e5d9
amoridanas <amoridanas5@gmail.com>
2025-01-16 19:14:02 +0100

    további lekérdezések hozzáadása a manage_courses.php-hoz

--- manage_courses.php
+++ manage_courses.php
@@ -14,3 +14,35 @@ $sql = "SELECT tananyag.id AS tananyag_id, tananyag.cim, tananyag.kategoria, use
         FROM tananyag
         JOIN users ON tananyag.szerzo = users.id";
 $result = $connection->query($sql);
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['tananyag_id'], $_POST['kozlemeny'])) {
+    $tananyag_id = intval($_POST['tananyag_id']);
+    $kozlemeny = $connection->real_escape_string($_POST['kozlemeny']);
+
+    $sql = "INSERT INTO kozlemenyek (tananyag_id, kozlemeny) VALUES (?, ?)";
+    $stmt = $connection->prepare($sql);
+    $stmt->bind_param("is", $tananyag_id, $kozlemeny);
+
+    if ($stmt->execute()) {
+        $message = "Közlemény sikeresen elküldve!";
+    } else {
+        $message = "Hiba történt a közlemény mentése során: " . $connection->error;
+    }
+    $stmt->close();
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['delete_tananyag_id'])) {
+    $tananyag_id = intval($_POST['delete_tananyag_id']);
+
+    $connection->query("DELETE FROM user_tananyag WHERE tananyag_id = $tananyag_id");
+    $connection->query("DELETE FROM kozlemenyek WHERE tananyag_id = $tananyag_id");
+
+    $sql = "DELETE FROM tananyag WHERE id = $tananyag_id";
+
+    if ($connection->query($sql) === TRUE) {
+        $message = "Kurzus sikeresen törölve!";
+    } else {
+        $message = "Hiba történt a kurzus törlése során: " . $connection->error;
+    }
+}
+?>
