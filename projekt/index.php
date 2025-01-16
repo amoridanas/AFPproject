b0594fc3e65e7c407f7fa15aa692ae75fbb5004f
amoridanas <amoridanas5@gmail.com>
2025-01-16 18:46:30 +0100

    index.php refaktorálása ismét

--- projekt/index.php
+++ projekt/index.php
@@ -21,7 +21,7 @@
     if (isset($_SESSION['user_id'])) {
         $userId = $_SESSION['user_id'];
         $sql = "SELECT permission FROM users WHERE id = ?";
-        $stmt = $conn->prepare($sql);
+        $stmt = $connection->prepare($sql);
         $stmt->bind_param("i", $userId);
         $stmt->execute();
         $result = $stmt->get_result();
